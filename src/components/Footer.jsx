a3840ce687abaf5f316073e46d57c1b013cc7ba0
Aniket Rana <121351449+AniketShirou@users.noreply.github.com>
2024-05-31 18:10:20 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="max-w-7xl mx-auto h-16 md:h-20 md:flex md:items-center md:justify-between">
+      <div className="text-lg text-gray-800 mr-4">
+      </div>
+      <div className="flex items-center justify-center">
+        <p className="text-gray-800"></p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
